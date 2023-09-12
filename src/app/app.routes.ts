08427c26a4100ca9d194c743aa97c7625d0b45b9
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2023-09-11 23:23:52 -0300

    creacion de componente y ruta de about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
 
+const title = 'Matias Galeano';
+
 export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () =>
       import('./components/home/home.component').then((c) => c.HomeComponent),
+    title: title + ' | Portfolio',
+  },
+  {
+    path: 'about-me',
+    loadComponent: () =>
+      import('./components/about-me/about-me.component').then(
+        (c) => c.AboutMeComponent
+      ),
+    title: title + ' | About Me',
   },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
