aad8d5929e049c0a2712e926507796d4e8b544b4
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2023-10-08 19:04:00 -0300

    datos de perfil en about conectados a datos de base de datos

--- src/app/pages/about-me/components/about-profile/about-profile.component.ts
+++ src/app/pages/about-me/components/about-profile/about-profile.component.ts
@@ -1,5 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule, ViewportScroller } from '@angular/common';
+import { FirebaseService } from '@shared/services/firebase.service';
+import { Profile } from '@shared/models/profile.model';
+import { AppService } from '@shared/services/app.service';
 
 @Component({
   selector: 'app-about-profile',
@@ -8,10 +11,33 @@ import { CommonModule, ViewportScroller } from '@angular/common';
   templateUrl: './about-profile.component.html',
   styleUrls: ['./about-profile.component.scss'],
 })
-export class AboutProfileComponent {
-  profilePic: string = 'assets/images/profilePicture.jpg';
+export class AboutProfileComponent implements OnInit {
+  profile!: Profile;
 
+  private firebase = inject(FirebaseService);
+  private app = inject(AppService);
   private viewportScroller = inject(ViewportScroller);
+
+  profilePic$ = this.firebase.getImageURL('profileImage');
+
+  ngOnInit(): void {
+    this.getProfileData();
+  }
+
+  getProfileData() {
+    const profile = sessionStorage.getItem('profile');
+    if (profile) {
+      this.profile = JSON.parse(profile);
+    } else {
+      this.app._loading$.next(true);
+      this.app.onInitApi().subscribe({
+        next: (res) => {
+          this.profile = res;
+          this.app._loading$.next(false);
+        },
+      });
+    }
+  }
 
   onClick(id: string) {
     this.viewportScroller.scrollToAnchor(id);
