b3ee11cb443fce4bca137fbf2bc09f0b60c98353
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2025-01-28 15:07:03 -0300

    carrusel para herramientas

--- src/app/pages/skills/components/tools-card/tools-card.component.ts
+++ src/app/pages/skills/components/tools-card/tools-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, effect, OnInit, signal } from '@angular/core';
 import {
   IonButton,
   IonCardContent,
@@ -22,48 +23,81 @@ import { constructOutline } from 'ionicons/icons';
     IonToolbar,
     IonCard,
     IonCardHeader,
-    IonIcon,
-    IonCardTitle,
-    IonButtons,
+    IonCardContent,
+    IonButton,
     IonLabel,
     IonItem,
     IonList,
-    IonCardContent,
-    IonButton,
+    IonIcon,
+    IonButtons,
+    IonCardTitle,
+    NgStyle,
   ],
   templateUrl: './tools-card.component.html',
   styleUrls: ['./tools-card.component.scss'],
 })
 export class ToolsCardComponent implements OnInit {
+  tools = signal([
+    {
+      name: 'Visual Studio Code',
+      icon: 'logoVisualstudio',
+      image: 'path_to_image/vscode.png',
+      description: 'Editor de código fuente',
+    },
+    {
+      name: 'Git',
+      icon: 'logoGit',
+      image: 'path_to_image/git.png',
+      description: 'Sistema de control de versiones',
+    },
+    {
+      name: 'Postman',
+      icon: 'logoPostman',
+      image: 'path_to_image/postman.png',
+      description: 'Herramienta para probar APIs',
+    },
+    {
+      name: 'Insomnia',
+      icon: 'logoInsomnia',
+      image: 'path_to_image/insomnia.png',
+      description: 'Alternativa de Postman',
+    },
+    {
+      name: 'Docker',
+      icon: 'logoDocker',
+      image: 'path_to_image/docker.png',
+      description:
+        'Plataforma para desarrollar, enviar y ejecutar aplicaciones en contenedores',
+    },
+  ]);
+
+  currentIndex = signal(0);
+
   constructor() {
     addIcons({
       constructOutline,
+    });
+    effect(() => {
+      const interval = setInterval(() => this.nextSlide(), 3000);
+      return () => clearInterval(interval);
     });
   }
 
   ngOnInit() {}
 
-  tools = [
-    {
-      name: 'Visual Studio Code',
-      icon: 'logoVisualstudio',
-    },
-    {
-      name: 'Git',
-      icon: 'logoGit',
-    },
+  prevSlide() {
+    const newIndex =
+      this.currentIndex() === 0
+        ? this.tools().length - 1
+        : this.currentIndex() - 1;
+    this.currentIndex.set(newIndex);
+  }
 
-    {
-      name: 'Postman',
-      icon: 'logoPostman',
-    },
-    {
-      name: 'Insomnia',
-      icon: 'logoInsomnia',
-    },
-    {
-      name: 'Docker',
-      icon: 'logoDocker',
-    },
-  ];
+  nextSlide() {
+    const newIndex =
+      this.currentIndex() === this.tools().length - 1
+        ? 0
+        : this.currentIndex() + 1;
+    this.currentIndex.set(newIndex);
+  }
 }
